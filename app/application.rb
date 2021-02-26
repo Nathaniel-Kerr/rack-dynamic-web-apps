533944a80a7cd8b2097072ec1fac22227af1a66e
Nathaniel_Kerr <NathanielKerr10@gmail.com>
2021-02-26 11:40:51 -0800

    Done.

--- app/application.rb
+++ app/application.rb
@@ -2,12 +2,12 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World "
+    resp.write "Hello, refresh the page to run slots! "
     resp.finish
 
-    num_1 = Kernel.rand(1..2)
-    num_2 = Kernel.rand(1..2)
-    num_3 = Kernel.rand(1..2)
+    num_1 = Kernel.rand(1..20)
+    num_2 = Kernel.rand(1..20)
+    num_3 = Kernel.rand(1..20)
 
     if num_1 == num_2 && num_2 == num_3
       nums = num_1, num_2, num_3
